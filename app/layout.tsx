470a5708c021b390be098e5ad1206ce289f054e4
Muhammad Ubaid Ullah <125487409+UbaidUllah2305@users.noreply.github.com>
2024-05-13 11:24:52 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] , variable: '--font-inter' });
+const ibmplexserief = IBM_Plex_Serif({ subsets: ["latin"], weight:['400','700'] ,variable: '--font-ibm-plex-serif' });
+
+export const metadata: Metadata = {
+  title: "Hrizon",
+  description: "Horizon is a Modern Banking platform for Everyone",
+  icons: {
+    icon: "/icons/logo.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmplexserief.variable}`}>{children}</body>
+    </html>
+  );
+}
